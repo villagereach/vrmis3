37b763f06b3d7e8c665fb4664c6a322d2ff6035c
Joel Hoffman <joel@sagan.(none)>
2010-03-18 14:41:33 -0700

    Adding User edit and update methods for Admin; ability to associate users with delivery zones and health centers and administrative areas

--- lib/controllers/users_controller.rb
+++ lib/controllers/users_controller.rb
@@ -6,10 +6,40 @@ class UsersController < OlmisController
   end
   
   
+  def index
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+    render :profile
+  end
+  
+  def update 
+    create
+  end
+
+  def create
+    if (request.put? || request.post?) && @current_user.admin?
+      User.transaction do
+        begin
+          @user = params[:id] ? User.find_by_id(params[:id]) : User.new(params[:user])
+          @user.save!
+          redirect_to profile_user_path(@user)
+        rescue ActiveRecord::ActiveRecordError
+          render :new
+        end
+      end
+    end
+  end
+  
   def profile
     @user = User.find_by_id(params[:id])
-    if request.put? || request.post?
-      redirect_to root_path and return if @user.update_attributes(params[:user])
+    if (request.put? || request.post?) && (@user == @current_user || @current_user.admin?) 
+      redirect_to profile_user_path(@user) and return if @user.update_attributes(params[:user])
     end
     @minimal_layout = true
   end
--- lib/olmis.rb
+++ lib/olmis.rb
@@ -21,7 +21,8 @@ class Olmis
         end
   
         definition['roles'].each do |l, options|
-          Role.find_or_initialize_by_code(name_to_code(l, 'Role')).update_attributes!(options.merge(:code => l))
+          code = name_to_code(l, 'Role')
+          Role.find_or_initialize_by_code(code).update_attributes!(options)
         end
   
         pt_by_code = {}
