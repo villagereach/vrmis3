27fdf9669dc93718b36ab491ee95fd353e82838f
David Cato <git@crunchyfrog.net>
2010-03-26 15:00:05 -0700

    Add submit_xform action

--- lib/controllers/data_sources_controller.rb
+++ lib/controllers/data_sources_controller.rb
@@ -1,7 +1,11 @@
 class DataSourcesController < OlmisController
   unloadable
 
-  add_breadcrumb 'breadcrumb.data_sources', 'data_sources_path'
+  skip_before_filter :check_logged_in, :only => [ :list_xforms, :submit_xform ]
+
+  protect_from_forgery :except => :submit_xform
+
+  add_breadcrumb 'breadcrumb.data_sources', 'data_sources_path', :except => [ :list_xforms, :submit_xform ]
 
   helper :visits
   
@@ -18,6 +22,30 @@ class DataSourcesController < OlmisController
   def list_xforms
     respond_to do |format|
       format.xml
+    end
+  end
+
+  def submit_xform
+    data = DataSubmission.new(
+      :user => current_user || User.admin,
+      :remote_ip => request.remote_ip)
+    
+    status, visit = data.handle_request(request)
+
+    respond_to do |format|
+      format.html do
+        if data.data_source.is_a?(AndroidOdkVisitDataSource)
+          headers['Location'] = xform_submit_url
+        else
+          headers['Location'] = data_sources_url
+        end
+        
+        if visit && status < 300 && !data.data_source.is_a?(AndroidOdkVisitDataSource)
+          redirect_to headers['Location']
+        else
+          render :nothing => true, :status => status
+        end
+      end
     end
   end
 end
