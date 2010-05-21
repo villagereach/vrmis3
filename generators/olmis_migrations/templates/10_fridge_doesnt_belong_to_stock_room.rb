0d01c62168262a711ba7cfa77f44456a07f9957d
Sam L <orionlee@yahoo.com>
2010-05-21 14:26:07 -0700

    fix on DB migration so that it works with a brand new schema. tested on mysql 5.0.51a

--- generators/olmis_migrations/templates/10_fridge_doesnt_belong_to_stock_room.rb
+++ generators/olmis_migrations/templates/10_fridge_doesnt_belong_to_stock_room.rb
@@ -1,9 +1,11 @@
 class OlmisFridgeDoesntBelongToStockRoom < ActiveRecord::Migration
   def self.up
-    drop_constraint :fridge_statuses, :stock_room_id, :references => :stock_rooms    
     add_column :fridge_statuses, :stock_room_id, :integer, :references => :stock_rooms
     execute 'update fridge_statuses set stock_room_id = (select stock_room_id from fridges where fridges.id = fridge_statuses.fridge_id)'
     change_column_null :fridge_statuses, :stock_room_id, false    
+    # the execute could be replaced by drop_constraint but the current drop_constraint is buggy. if drop_constraint works, it can be something like the following
+    #drop_constraint :fridges, {:name => :fridges_stock_room_id_fkey, :foreign_key => true} 
+    execute "ALTER TABLE fridges DROP FOREIGN KEY fridges_stock_room_id_fkey"
     remove_column :fridges, :stock_room_id
   end
 
