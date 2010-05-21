54690373165e6154ca3fae94b1c7fea8dd34a533
Sam L <orionlee@yahoo.com>
2010-05-21 14:14:17 -0700

    install fixes at template level; make it more robust too (fail-fast)

---
+++ sample/olmis_minimal.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+  'languages' => ['en'],
+  'time_zone' => 'Pacific Time (US & Canada)',
+
+  'roles' => {
+    'admin'            => { :landing_page => '/' },
+    'manager'            => { :landing_page => '/' },
+    'observer'           => { :landing_page => '/observer' },
+    'field_coordinator'  => { :landing_page => '/' },
+  },
+}
