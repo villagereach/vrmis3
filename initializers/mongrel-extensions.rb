6cf57b530172fcdde7fbf0722a3676f90293a267
David Cato <git@crunchyfrog.net>
2010-06-21 15:59:58 -0700

    Override Mongrel::DirHandler only if it's defined

--- initializers/mongrel-extensions.rb
+++ initializers/mongrel-extensions.rb
@@ -1,9 +1,11 @@
-module Mongrel
-  class DirHandler
-    def send_file_with_cache_control(req_path, request, response, header_only=false)
-      response.header['Cache-Control'] = 'max-age=0, must-revalidate'
-      send_file_without_cache_control(req_path, request, response, header_only)
+if defined? Mongrel::DirHandler
+  module Mongrel
+    class DirHandler
+      def send_file_with_cache_control(req_path, request, response, header_only=false)
+        response.header['Cache-Control'] = 'max-age=0, must-revalidate'
+        send_file_without_cache_control(req_path, request, response, header_only)
+      end
+      alias_method_chain :send_file, :cache_control
     end
-    alias_method_chain :send_file, :cache_control
   end
 end
